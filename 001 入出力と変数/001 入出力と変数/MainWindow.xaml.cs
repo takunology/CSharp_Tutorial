34e7be5c6f9de0df2ded9d86539b4be2ae7e246e
Takunology <takunology.net@gmail.com>
2019-05-23 13:47:14 +0900

    tutorial

---
+++ 001 入出力と変数/001 入出力と変数/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _001_入出力と変数
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //文字列型(string)の変数(入れ物)を用意
+            string text;
+
+            //textにtextboxから得られた文字を格納
+            text = textbox.Text;
+
+            //textをMessageboxで表示する
+            MessageBox.Show("あなたが入力した文字は「" + text + "」です。");
+        }
+    }
+}
---
+++ 002 簡易計算ツール/002 簡易計算ツール/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _002_簡易計算ツール
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        int sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        int app(int x, int y)
+        {
+            return x * y;
+        }
+
+        int div(int x, int y)
+        {
+            return x / y;
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+            //int型に変換する -> int.Parse() メソッド(関数)
+            int x = int.Parse(textbox1.Text);
+            int y = int.Parse(textbox2.Text);
+
+            //add関数にtextboxの値を渡す
+            //add関数(加算関数)から受け取った値(return)を ans 変数に格納
+            int ans = add(x, y);
+
+            //Messagebox は文字列型なので .ToString() メソッドで変換
+            MessageBox.Show("答えは " + ans.ToString() + " です。");
+            
+        }
+
+        private void Button_Sub(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(textbox1.Text);
+            int y = int.Parse(textbox2.Text);
+
+            int ans = sub(x, y);
+
+            MessageBox.Show("答えは " + ans.ToString() + " です。");
+        }
+
+        private void Button_App(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(textbox1.Text);
+            int y = int.Parse(textbox2.Text);
+
+            int ans = app(x, y);
+
+            MessageBox.Show("答えは " + ans.ToString() + " です。");
+        }
+
+        private void Button_Div(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(textbox1.Text);
+            int y = int.Parse(textbox2.Text);
+
+            int ans = div(x, y);
+
+            MessageBox.Show("答えは " + ans.ToString() + " です。");
+        }
+    }
+}
