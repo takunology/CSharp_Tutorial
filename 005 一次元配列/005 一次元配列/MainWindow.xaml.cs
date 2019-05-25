128eba1167da23ed71ecd7f300c71f7c4f6a0f8d
takunology <m15025@g.metro-cit.ac.jp>
2019-05-26 00:26:55 +0900

    ロジック部分をコーディング

--- 005 一次元配列/005 一次元配列/MainWindow.xaml.cs
+++ 005 一次元配列/005 一次元配列/MainWindow.xaml.cs
@@ -24,5 +24,27 @@ namespace _005_一次元配列
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //配列は[]で囲み、newで初期化
+            //4は要素の数を指定している
+            string[] data = new string[4];
+            
+            data[0] = msg1.ToString();
+            data[1] = msg2.ToString();
+            data[2] = msg3.ToString();
+            data[3] = "配列データ4つ目はロジック側で設定してみた！";
+
+            object a = msg1;
+
+            //配列名.Length は配列の要素数を返す
+            //要素数の数だけ繰り返す
+            for(int i = 0; i < data.Length; i++)
+            {
+                MessageBox.Show(data[i]);
+            }
+
+        }
     }
 }
