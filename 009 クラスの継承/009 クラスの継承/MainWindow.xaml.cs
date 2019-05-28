c13c2ea9752a40f728d6e3b8c6df8af65431b387
takunology <m15025@g.metro-cit.ac.jp>
2019-05-29 03:02:26 +0900

    クラスの継承を使うWPFアプリです

---
+++ 009 クラスの継承/009 クラスの継承/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _009_クラスの継承
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Show_Student_Data ssd = new Show_Student_Data();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Show(object sender, RoutedEventArgs e)
+        {
+            ssd.Show();
+        }
+
+        private void Button_Write(object sender, RoutedEventArgs e)
+        {
+            ssd.Write();
+        }
+
+        private void Text_NumOnlyInput(object sender, TextCompositionEventArgs e)
+        {
+            //入力できる値を0～9までにする
+            e.Handled = !new Regex("[0-9]").IsMatch(e.Text);
+        }
+
+        private void Text_NumOnlyExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            //貼り付け(ペースト)による数値以外の入力を拒否
+            if (e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+
+    public class Show_Student_Data : Student //Studentを継承
+    {
+        //親クラスのインスタンス化
+        Student student = new Student();
+
+        public void Show()
+        {
+            var mainWindow = (MainWindow)App.Current.MainWindow;
+
+            //親クラスの表示メソッドを呼び出す
+            //ラベルにデータを表示する
+            mainWindow.show_data.Content = "=== 開示データ ===\n";
+            mainWindow.show_data.Content += student.Student_Show();
+        }
+
+        public void Write()
+        {
+            var mainWindow = (MainWindow)App.Current.MainWindow;
+
+            string name = mainWindow.Name_box.Text;
+            int grade = int.Parse(mainWindow.Grade_box.Text);
+            int number = int.Parse(mainWindow.Number_box.Text);
+            
+            //親クラスの書き込みメソッドを呼び出す
+            student.Student_Write(grade, number, name);
+
+            MessageBox.Show("書き込みました。");
+        }
+    }
+}
---
+++ 009 クラスの継承/009 クラスの継承/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_クラスの継承
+{
+    public class Student
+    {
+        int grade; //学年
+        int number; //番号
+        string name; //名前
+
+        //書き込み用メソッド
+        public void Student_Write(int x, int y, string s)
+        {
+            grade = x;
+            number = y;
+            name = s;
+        }
+
+        //文字列のかたまりを返り値にする
+        public string Student_Show()
+        {
+            return " 名前 " + name + "\n 学年 " + grade.ToString()
+                        + "\n 番号 " + number.ToString();
+        }
+    }
+}
