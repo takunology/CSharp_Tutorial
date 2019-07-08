370101784456786293b2306744df397e61fea078
takunology <m15025@g.metro-cit.ac.jp>
2019-07-09 02:01:43 +0900

    アクセシビリティを使ったカプセル化

---
+++ 015 アクセシビリティ/015 アクセシビリティ/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace _015_アクセシビリティ
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //クラスをインスタンス化(実体化)
+        Human human = new Human();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void 登録(object sender, RoutedEventArgs e)
+        {
+            // Human クラスの public メソッドへアクセス
+            human.身長入力(height.Text);
+            human.体重入力(weight.Text);
+            human.年齢入力(age.Text);
+            human.名前入力(name.Text);
+
+            MessageBox.Show("人間データを登録しました");
+        }
+
+        private void 年をとる(object sender, RoutedEventArgs e)
+        {
+            // Human クラスの年を取るメソッドを実行
+            // 引数はテキストボックスから得た値
+            human.年を取る();
+            MessageBox.Show("あれから1年が経った...");
+        }
+
+        private void 情報開示(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(human.表示());
+        }
+    }
+}
---
+++ 015 アクセシビリティ/015 アクセシビリティ/human.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+
+namespace _015_アクセシビリティ
+{
+    class Human
+    {
+        // 直接ユーザからいじられないように保護する
+        private int 身長;
+        private int 体重;
+        private int 年齢;
+        private string 名前;
+
+        public int isInt = 0; //int型かどうかの判定後に代入
+
+        // ここにユーザからのデータを入力してもらう
+        // 特に返り値はないので void 
+        public void 身長入力(string value)
+        {
+            bool result = int.TryParse(value, out isInt);
+            if (result == true)
+            {
+                this.身長 = isInt;
+            }
+            else
+            {
+                MessageBox.Show("整数を入力してください");
+            }
+        }
+
+        public void 体重入力(string value)
+        {
+            bool result = int.TryParse(value, out isInt);
+            if (result == true)
+            {
+                this.体重 = isInt;
+            }
+            else
+            {
+                MessageBox.Show("整数を入力してください");
+            }
+        }
+
+        public void 年齢入力(string value)
+        {
+            bool result = int.TryParse(value, out isInt);
+            if (result == true)
+            {
+                this.年齢 = isInt;
+            }
+            else
+            {
+                MessageBox.Show("整数を入力してください");
+            }
+        }
+
+        public void 名前入力(string name)
+        {
+            this.名前 = name;
+        }
+
+        public void 年を取る()
+        {
+            this.年齢 = this.年齢 + 1;
+        }
+
+        public string 表示()
+        {
+            return $"身長：{this.身長} \n体重：{this.体重} \n名前：{this.名前} \n年齢：{this.年齢}";
+        }
+    }
+}
