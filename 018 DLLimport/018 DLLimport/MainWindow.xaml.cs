78a88a2edec650a997c2d8252a6eebf8ef5124fc
takunology <m15025@g.metro-cit.ac.jp>
2019-11-10 17:44:29 +0900

    DLLファイルの読み込み

---
+++ 018 DLLimport/018 DLLimport/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _018_DLLimport
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Multi(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Sub(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Div(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
